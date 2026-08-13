dbc5979c1526ef479dfb2957ab738a13635db857
khennyboy <abdullateefkehinde848@gmail.com>
2026-08-13 10:53:29 +0100

    deleted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import MenuPage from "./pages/MenuPage";
-import RoomsPage from "./pages/RoomsPage";
-
-import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/rooms" element={<RoomsPage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
