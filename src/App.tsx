3d2c355943c515fe12c15f7908e63ca725a363c5
khennyboy <abdullateefkehinde848@gmail.com>
2026-08-16 10:03:16 +0100

    added meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavFooter from "./component/NavFooter";
 import EachRoom from "./component/EachRoom";
+import ScrollToTop from "./component/ScrolltoTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<NavFooter />}>
           <Route index element={<HomePage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
-import { HelmetProvider } from "react-helmet-async";
-import ScrollToTop from "./component/ScrolltoTop.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Provider>
-        <ScrollToTop />
+     
         <App />
       </Provider>
     </HelmetProvider>
