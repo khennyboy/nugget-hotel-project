e06357161fb9ef693216d6e1f9ea4b8990d0ebfc
khennyboy <abdullateefkehinde848@gmail.com>
2026-08-16 09:11:26 +0100

    added meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import { HelmetProvider } from "react-helmet-async";
+import ScrollToTop from "./component/ScrolltoTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <Provider>
+        <ScrollToTop />
         <App />
       </Provider>
     </HelmetProvider>
